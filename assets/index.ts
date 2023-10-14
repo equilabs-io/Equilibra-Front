f78d9451f1963a1efdd9a441e60ad0f50c7e1f7b
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-10-14 00:01:35 -0300

    add navigation links & assets

---
+++ assets/index.ts
@@ -0,0 +1,5 @@
+import logoPng from "./equilibra-logo.png";
+import LogoSvg from "./EquilibraLogo";
+
+export { logoPng };
+export default LogoSvg;
---
+++ constants/navigation.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    name: "Create project",
+    link: "/app/create-project",
+  },
+  {
+    name: "Create pool",
+    link: "/app/create-pool",
+  },
+  {
+    name: "Support projects",
+    link: "/app/projects",
+  },
+];
