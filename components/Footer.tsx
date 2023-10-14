169b24d6cc7dcde50a00466a6147e17d848d6fce
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-10-14 00:02:11 -0300

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="mx-auto w-full flex flex-col max-w-7xl items-center justify-between p-6 lg:px-8">
+      <div className="text-primary">
+        © 2023 Equilibra, All Rights Reserved
+      </div>
+    </footer>
+  );
 }
