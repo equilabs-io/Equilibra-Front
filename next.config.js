a7c178c52439b8f728a3aa03974cc199166cb08e
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-10-20 14:11:19 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [process.env.PINATA_GATEWAY],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
