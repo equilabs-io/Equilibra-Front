770761e695b7cc177a65dc93f9c286e7d5716aca
Matias <mantitaaa89@gmail.com>
2023-11-04 18:11:19 -0300

    Add the following: - `PoolsPage` wrap layout + mockup data. - Update `PoolCard` comp. - Delete PoolLayout comp.

--- app/demo/pools/page.tsx
+++ app/demo/pools/page.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 import PoolCard from "@/components/Pools/PoolCard";
-export default function Pools() {
+export default function Pools(): React.ReactElement {
+  const pools = [
+    {
+      name: "Pool Name #1",
+      owner: "0x5B...0Fa4dE3",
+      fundingToken: "ETHx",
+      governanceToken: "ETH",
+    },
+    {
+      name: "Pool Name #2",
+      owner: "0x5B...0Taj93",
+      fundingToken: "DAIx",
+      governanceToken: "ETH",
+    },
+    {
+      name: "Pool Name #3",
+      owner: "0x5B...HHHHb3",
+      fundingToken: "USDCx",
+      governanceToken: "ETH",
+    },
+  ];
   return (
     <>
-      <PoolCard />
-      <div className="w-full">{/* <PoolsLayout pools={pools} /> */}</div>
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 w-full justify-center"
+      >
+        {pools.map((pool) => (
+          <li
+            key={pool.name}
+            className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-surface  overflow-hidden"
+          >
+            <div className="flex flex-1 flex-col p-1">
+              <PoolCard {...pool} />
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
--- components/Pools/PoolCard.tsx
+++ components/Pools/PoolCard.tsx
@@ -10,21 +10,26 @@ interface PoolCardProps {
 
 const PoolCard: React.FC<PoolCardProps> = ({
   name = "Pool Name",
-  owner,
+  owner = "0x5B...0Fa4dE3",
   fundingToken,
   governanceToken,
   onDepositClick,
 }) => {
+  const extractColor = (hexStr: string) => `#${hexStr.substring(2, 8)}`;
+  const color = extractColor("0x4BE8Bb8d7923879c3DDc9c551C5Aa85Ad0Fa4dE3");
+
   return (
     <>
-      <div className="rounded-2xl overflow-hidden max-w-[320px] hover:shadow-sm hover:shadow-[#00FF9D] cursor-pointer">
+      <div className="rounded-2xl overflow-hidden hover:shadow-sm hover:shadow-[#00FF9D] cursor-pointer">
         <div className="bg-[#0C0C0E] rounded-lg shadow">
-          <div className="min-h-[170px] bg-gradient-to-r from-cyan-600 to-blue-700"></div>
+          <div
+            className={`min-h-[170px] bg-gradient-to-r from-cyan-600 to-[${color}]`}
+          ></div>
           {/* Image Here */}
           <div className="px-8 py-7">
             <div className=" mb-5">
               <h3 className="text-2xl font-bold text-primary tracking-tight">
-                {name}
+                {owner}
               </h3>
               <p className="text-xs text-grey_light">@poolName</p>
             </div>
@@ -35,13 +40,14 @@ const PoolCard: React.FC<PoolCardProps> = ({
               natus.
             </p>
             <div className="mb-12">
-              <h4 className="text-xs text-grey_light">
-                Governance Token: icon
-              </h4>
+              <h5 className="text-xs text-grey_light">
+                Governance Token: {governanceToken}
+              </h5>
             </div>
             <div className="">
-              <button className="inline-flex  px-3 py-2 text-md font-bold text-background bg-primary rounded-lg w-full text-center items-center justify-center">
+              <button className="inline-flex  px-3 py-2 text-md font-bold text-background bg-primary rounded-lg w-full items-center justify-center">
                 50.000
+                <span className="h-4 w-4 border-2 rounded-full ml-2 border-slate-900"></span>
               </button>
             </div>
           </div>
