1d65849741d7437ede7554255c8ed757b8bcba96
Matias <mantitaaa89@gmail.com>
2023-12-12 16:32:39 -0300

    feat: add state style to `AddToList` comp

--- components/Project/AddListButton.tsx
+++ components/Project/AddListButton.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+import { useState } from "react";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 type AddListButtonProps = {
   active: boolean;
   id: string;
@@ -7,19 +8,29 @@ type AddListButtonProps = {
 };
 
 const AddListButton = ({ active, id, projectCheckout }: AddListButtonProps) => {
+  const [clickedOn, setClickedOn] = useState(false);
+
+  const hanlde = (id: any) => {
+    projectCheckout(id);
+    setClickedOn(true);
+  };
+
   return (
     <>
       <div className="absolute right-4 top-4  flex  justify-end">
         <button
           disabled={active}
-          onClick={() => projectCheckout(id)}
-          className={`w-full min-w-[170px] rounded-md px-4 ${
-            active
-              ? "bg-primary text-black"
-              : "bg-surface text-white hover:bg-primary hover:text-black"
-          }`}
+          onClick={() => hanlde(id)}
+          className={`flex w-full min-w-[170px] items-center justify-center rounded-md px-4 transition-all duration-200 ease-in ${
+            active ? "bg-primary text-black" : "bg-surface hover:bg-primary"
+          }, ${
+            clickedOn
+              ? "justify-evenly bg-transparent hover:bg-transparent"
+              : "text-surface_var hover:text-white"
+          } `}
         >
-          {`${active ? "Listed" : "Add to list"}`}
+          {`${active ? "Listed" : clickedOn ? "Added" : "Add to Cart"}`}
+          {clickedOn && <CheckCircleIcon className="h-6 w-6 text-primary" />}
         </button>
       </div>
     </>
